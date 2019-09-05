106c12c033e0afaabedb8118c6c0d99c1cb96bc6
Jake Jarvis <jake@jarv.is>
2019-09-05 18:04:38 -0400

    Clean up JSON parsing

--- Dockerfile
+++ Dockerfile
@@ -13,13 +13,13 @@ LABEL maintainer="Jake Jarvis <jake@jarv.is>"
 # Install and upgrade utilities
 RUN apt-get update --fix-missing && apt-get -y upgrade
 
-# Install latest Chrome stable (and jq for later JSON parsing -- https://stedolan.github.io/jq/)
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt-get update \
-    && apt-get install -y google-chrome-stable jq bc --no-install-recommends \
-    && rm -rf /var/lib/apt/lists/* \
-    && rm -rf /src/*.deb
+# Install latest Chrome stable (and jq/bc for later JSON parsing -- https://stedolan.github.io/jq/)
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+    apt-get update && \
+    apt-get install -y google-chrome-stable jq bc --no-install-recommends && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /src/*.deb
 
 # Download latest Lighthouse build from npm
 # Cache bust to ensure latest version when building the image
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,18 @@ set -e
 
 mkdir report
 
-lighthouse --port=9222 --chrome-flags="--headless --disable-gpu --no-sandbox --no-zygote" --output "html" --output "json" --output-path "report/lighthouse" ${INPUT_URL}
+set -- lighthouse --port=9222 --chrome-flags="--headless --disable-gpu --no-sandbox --no-zygote" --output "html" --output "json" --output-path "report/lighthouse" ${INPUT_URL}
 
 # Parse individual scores from JSON output
 # Unorthodox jq syntax because of dashes -- https://github.com/stedolan/jq/issues/38
-SCORE_PERFORMANCE=$(cat ./report/lighthouse.report.json | jq '.categories["performance"].score')
-SCORE_ACCESSIBILITY=$(cat ./report/lighthouse.report.json | jq '.categories["accessibility"].score')
-SCORE_PRACTICES=$(cat ./report/lighthouse.report.json | jq '.categories["best-practices"].score')
-SCORE_SEO=$(cat ./report/lighthouse.report.json | jq '.categories["seo"].score')
-SCORE_PWA=$(cat ./report/lighthouse.report.json | jq '.categories["pwa"].score')
+SCORE_PERFORMANCE=$(jq '.categories["performance"].score' ./report/lighthouse.report.json)
+SCORE_ACCESSIBILITY=$(jq '.categories["accessibility"].score' ./report/lighthouse.report.json)
+SCORE_PRACTICES=$(jq '.categories["best-practices"].score' ./report/lighthouse.report.json)
+SCORE_SEO=$(jq '.categories["seo"].score' ./report/lighthouse.report.json)
+SCORE_PWA=$(jq '.categories["pwa"].score' ./report/lighthouse.report.json)
 
 # Print scores to standard output (out of 100 instead of 0 to 1)
-# Using hacky bc b/c bash hates floating point arithmetic
+# TO DO: Don't use hacky bc b/c bash hates floating point arithmetic
 printf "%s\n" "----------------------------------"
 printf "|  Performance:           %.0f\t|\n" $(echo "$SCORE_PERFORMANCE*100" | bc -l)
 printf "|  Accessibility:         %.0f\t|\n" $(echo "$SCORE_ACCESSIBILITY*100" | bc -l)
