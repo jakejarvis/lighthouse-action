2b3884b8a778b5239be0cb6bbaeb057a00a4cc8c
Sindre Bøyum <sindre.boyum@gmail.com>
2020-05-13 10:31:45 +0200

    Post lighthouse results as a comment on the pr

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,7 @@ printf "    %s\n" "$OUTPUT_PATH.report.html" > message
 printf "    %s\n" "$OUTPUT_PATH.report.json" > message
 
 printf message
+printf "test"
 
 json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(json)
